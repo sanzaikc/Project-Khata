453d35a8cbeaf4b587cadd2048ac2e3f1cd3a626
sanzaikc <sanzaikc@gmail.com>
2019-09-23 20:02:54 +0545

    enhanced UI

--- script.js
+++ script.js
@@ -14,20 +14,3 @@ function openTab(event, tabName) {
   event.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
-
-// Dropdown onclick
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
